a11e33f954caf75269952353916049440951a126
Luke Iannini <lukexi@me.com>
2012-11-12 02:39:04 -0800

    Updated Xcode iOS SDK version to 6.0 to match latest Xcode release

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export TARGET_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk
+export TARGET_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.0.sdk
 export TARGET_BIN="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin"
 export TARGET_GCC=$TARGET_BIN/arm-apple-darwin10-llvm-gcc-4.2
 export TARGET_CLANG=clang
@@ -8,7 +8,7 @@ export TARGET_CPPFLAGS="-I$TARGET_PLATFORM/usr/include/"
 export TARGET_CFLAGS="-isysroot $TARGET_PLATFORM -march=armv7 -mcpu=cortex-a8 -mfpu=neon"
 export TARGET_LDFLAGS="-L$TARGET_PLATFORM/usr/lib/"
 
-export LOCAL_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.1.sdk
+export LOCAL_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator6.0.sdk
 export LOCAL_BIN="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin"
 export LOCAL_GCC=$LOCAL_BIN/i686-apple-darwin11-llvm-gcc-4.2
 export LOCAL_LD=$TARGET_GCC
