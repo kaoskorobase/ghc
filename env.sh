0e7c84d021d3fa873b146b661181956335aa52bf
Stefan Kersten <kaoskorobase@gmail.com>
2012-12-18 12:25:09 +0100

    Allow to specify different SDK versions

--- env.sh
+++ env.sh
@@ -1,6 +1,14 @@
-export TARGET_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.0.sdk
-export TARGET_BIN="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin"
-export TARGET_GCC=$TARGET_BIN/arm-apple-darwin10-llvm-gcc-4.2
+DEVELOPER_DIR=`xcode-select -print-path`
+SDK_VERSION=${SDK_VERSION:-6.0}
+
+case $SDK_VERSION in
+	5.*) TARGET_DARWIN_VERSION=10 ; LOCAL_DARWIN_VERSION=10 ; GCC_VERSION=4.2 ;;
+	6.*) TARGET_DARWIN_VERSION=10 ; LOCAL_DARWIN_VERSION=11 ; GCC_VERSION=4.2 ;;
+esac
+
+export TARGET_PLATFORM="$DEVELOPER_DIR/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${SDK_VERSION}.sdk"
+export TARGET_BIN="$DEVELOPER_DIR/Platforms/iPhoneOS.platform/Developer/usr/bin"
+export TARGET_GCC=$TARGET_BIN/arm-apple-${TARGET_DARWIN_VERSION}-llvm-gcc-${GCC_VERSION}
 export TARGET_CLANG=clang
 export TARGET_LD=$TARGET_CLANG
 
@@ -8,16 +16,16 @@ export TARGET_CPPFLAGS="-I$TARGET_PLATFORM/usr/include/"
 export TARGET_CFLAGS="-isysroot $TARGET_PLATFORM -march=armv7 -mcpu=cortex-a8 -mfpu=neon"
 export TARGET_LDFLAGS="-L$TARGET_PLATFORM/usr/lib/"
 
-export LOCAL_PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator6.0.sdk
-export LOCAL_BIN="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin"
-export LOCAL_GCC=$LOCAL_BIN/i686-apple-darwin11-llvm-gcc-4.2
+export LOCAL_PLATFORM="$DEVELOPER_DIR/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator${SDK_VERSION}.sdk"
+export LOCAL_BIN="$DEVELOPER_DIR/Platforms/iPhoneSimulator.platform/Developer/usr/bin"
+export LOCAL_GCC=$LOCAL_BIN/i686-apple-${LOCAL_DARWIN_VERSION}-llvm-gcc-${GCC_VERSION}
 export LOCAL_LD=$TARGET_GCC
 export LOCAL_CPPFLAGS="-I$LOCAL_PLATFORM/usr/include/"
 export LOCAL_CFLAGS="-isysroot $LOCAL_PLATFORM -march=i386"
 export LOCAL_LDFLAGS="-L$LOCAL_PLATFORM/usr/lib/"
 
-sudo mkdir -p /Developer/usr/bin
-sudo rm -f /Developer/usr/bin/ar
-sudo ln -s /usr/bin/ar /Developer/usr/bin/ar
+##sudo mkdir -p /Developer/usr/bin
+##sudo rm -f /Developer/usr/bin/ar
+##sudo ln -s /usr/bin/ar /Developer/usr/bin/ar
 
 export PATH="`pwd`/bin:$PATH"
